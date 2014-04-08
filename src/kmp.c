14a42f9184a066750dc01d6e29fe71f3b1dc3b5b
Tuomas Lappeteläinen <tuomas.lappetelainen@aalto.fi>
2014-04-08 20:19:07 +0300

    kmp: initial implementation

--- src/kmp.c
+++ src/kmp.c
@@ -25,13 +25,43 @@ int kmp_build_border(const char *pattern, int32_t *border)
         i++;
     }
 
+    /* TODO: remove return value */
     return 0;
 }
 
 int kmp_match(const char *text, const char *pattern)
 {
-    (void)text;
-    (void)pattern;
+    if (text == NULL || pattern == NULL) {
+        return -1;
+    }
+
+    int32_t border[strlen(pattern) + 1];
+
+    kmp_build_border(pattern, &(border[1]));
+
+    border[0] = -1;
+
+    const size_t text_len = strlen(text);
+    const size_t pattern_len = strlen(pattern);
+
+    uint32_t i = 0;
+    uint32_t match = 0;
+
+    while (match + i < text_len) {
+        if (pattern[i] == text[match + i]) {
+            i++;
+            if (i == pattern_len) {
+                return (int)match;
+            }
+        } else {
+            match = match + i - border[i];
+            if (border[i] >= 0) {
+                i = (uint32_t)border[i];
+            } else {
+                i = 0;
+            }
+        }
+    }
 
     return -1;
 }
