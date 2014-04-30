1d90307a06407068ffda780369cebfb0f226bd06
Tuomas Lappeteläinen <tuomas.lappetelainen@aalto.fi>
2014-04-30 11:42:06 +0300

    bm: add stubs

---
+++ src/bm.c
@@ -0,0 +1,5 @@
+int bm_build_good_suffix(const char *pattern, int *good_suffix);
+
+int bm_build_bad_char(const char *pattern, int *bad_char);
+
+int bm_match(const char *text, const char *pattern);
---
+++ src/bm.h
@@ -0,0 +1,10 @@
+#ifndef ALGORITHMS_BM_H
+#define ALGORITHMS_BM_H
+
+int bm_build_good_suffix(const char *pattern, int *good_suffix);
+
+int bm_build_bad_char(const char *pattern, int *bad_char);
+
+int bm_match(const char *text, const char *pattern);
+
+#endif /* ALGORITHMS_BM_H */
